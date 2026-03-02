fdbdfc6f312dc96e6afc370bde0d96d4ee1e305e
Federico Cantore <f.cantore85@gmail.com>
2026-03-02 16:01:03 +0100

    feat: change endpoint name

--- src/main/java/cat/itacademy/s04/t01/userapi/controller/HealthController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controller/HealthController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
 
     @GetMapping("/health")
-    public String printText() {
+    public String printOk() {
         return "OK";
     }
 }
