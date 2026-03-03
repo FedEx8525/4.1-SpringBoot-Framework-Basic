f28a415441f06521a62a1a48530b5ae6b463fd47
Federico Cantore <f.cantore85@gmail.com>
2026-03-03 11:35:44 +0100

    feat: add POST endpoint to create urers in a ArrayList

--- src/main/java/cat/itacademy/s04/t01/userapi/controller/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controller/UserController.java
@@ -1,12 +1,11 @@
 package cat.itacademy.s04.t01.userapi.controller;
 
 import cat.itacademy.s04.t01.userapi.entity.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
@@ -17,5 +16,12 @@ public class UserController {
     @GetMapping
     public List<User> getAllUsers(){
         return users;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        user.setId(UUID.randomUUID());
+        users.add(user);
+        return user;
     }
 }
