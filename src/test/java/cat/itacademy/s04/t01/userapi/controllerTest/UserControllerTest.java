02fc2b5116d394d506b5f0c7a7e9a41898293632
Federico Cantore <f.cantore85@gmail.com>
2026-03-03 19:23:37 +0100

    feat: add @BeforeEach to clean list user before each test
    feat: add createUser_returnsUserWithId() test

--- src/test/java/cat/itacademy/s04/t01/userapi/controllerTest/UserControllerTest.java
+++ src/test/java/cat/itacademy/s04/t01/userapi/controllerTest/UserControllerTest.java
@@ -1,14 +1,19 @@
 package cat.itacademy.s04.t01.userapi.controllerTest;
 
 import cat.itacademy.s04.t01.userapi.controller.UserController;
+import cat.itacademy.s04.t01.userapi.entity.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -21,11 +26,30 @@ public class UserControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @BeforeEach
+    void setup() {
+        UserController.users.clear();
+    }
+
     @Test
     void getUsers_returnEmptyListInitially() throws Exception {
         mockMvc.perform(get("/users"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    void createUser_returnsUserWithId() throws Exception {
+        User user = new User();
+        user.setName(("Anna"));
+        user.setEmail("anna@mail.com");
+
+        mockMvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.name").value("Anna"));
     }
 
 
