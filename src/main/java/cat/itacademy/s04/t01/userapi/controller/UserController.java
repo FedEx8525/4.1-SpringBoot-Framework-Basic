ff29063e6350c0373e434a0134222b2cc8d3b34d
Federico Cantore <f.cantore85@gmail.com>
2026-03-03 11:18:41 +0100

    feat: add UserController
    feat: add getAllUser() endpoint

--- src/main/java/cat/itacademy/s04/t01/userapi/controller/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controller/UserController.java
@@ -1,4 +1,21 @@
 package cat.itacademy.s04.t01.userapi.controller;
 
+import cat.itacademy.s04.t01.userapi.models.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private static final List<User> users = new ArrayList<User>();
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return users;
+    }
 }
