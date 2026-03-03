01ff58a14179ef2bacedd661e265ae8e9f40922f
Federico Cantore <f.cantore85@gmail.com>
2026-03-03 18:55:25 +0100

    feat: add @BeforeEach to clean list users befor each test

--- src/test/java/cat/itacademy/s04/t01/userapi/controllerTest/HealthControllerTest.java
+++ src/test/java/cat/itacademy/s04/t01/userapi/controllerTest/HealthControllerTest.java
@@ -1,6 +1,8 @@
 package cat.itacademy.s04.t01.userapi.controllerTest;
 
 import cat.itacademy.s04.t01.userapi.controller.HealthController;
+import cat.itacademy.s04.t01.userapi.controller.UserController;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -15,6 +17,11 @@ public class HealthControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+        UserController.users.clear();
+    }
 
     @Test
     void shouldReturnOkStatus() throws Exception {
