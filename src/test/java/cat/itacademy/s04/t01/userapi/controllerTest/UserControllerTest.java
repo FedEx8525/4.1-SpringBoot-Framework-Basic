dfc347e4827a325ea25467be19c8f5fba6b00ecc
Federico Cantore <f.cantore85@gmail.com>
2026-03-03 18:25:07 +0100

    feat: add getUsers_returnEmptyListInitially() test

--- src/test/java/cat/itacademy/s04/t01/userapi/controllerTest/UserControllerTest.java
+++ src/test/java/cat/itacademy/s04/t01/userapi/controllerTest/UserControllerTest.java
@@ -1,4 +1,33 @@
 package cat.itacademy.s04.t01.userapi.controllerTest;
 
+import cat.itacademy.s04.t01.userapi.controller.UserController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
 public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void getUsers_returnEmptyListInitially() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+
+
 }
