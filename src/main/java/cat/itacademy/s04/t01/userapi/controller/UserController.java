5f4a064e1fe448506b520df4b53b8aad5488f4e1
Federico Cantore <f.cantore85@gmail.com>
2026-03-04 18:15:19 +0100

    feat: Injected urserRepository in UserController by constructor

--- src/main/java/cat/itacademy/s04/t01/userapi/controller/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controller/UserController.java
@@ -2,9 +2,9 @@ package cat.itacademy.s04.t01.userapi.controller;
 
 import cat.itacademy.s04.t01.userapi.entity.User;
 import cat.itacademy.s04.t01.userapi.exception.NotFoundByIdException;
+import cat.itacademy.s04.t01.userapi.repository.InMemoryUserRepository;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -12,8 +12,17 @@ import java.util.UUID;
 @RequestMapping("/users")
 public class UserController {
 
+    private final InMemoryUserRepository userRepository;
+
+    // Inyección por constructor (Buena práctica)
+    public UserController(InMemoryUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping
     public List<User> getUsers(@RequestParam(required = false) String name){
+        List<User> users = userRepository.findAll();
+
         if(name == null || name.isEmpty()) {
             return users;
         }
@@ -26,13 +35,13 @@ public class UserController {
     @PostMapping
     public User createUser(@RequestBody User user){
         user.setId(UUID.randomUUID());
-        users.add(user);
+        userRepository.save(user);
         return user;
     }
 
     @GetMapping("/{id}")
     public User getUserByID(@PathVariable UUID id){
-        return users.stream()
+        return userRepository.findAll().stream()
                 .filter(user -> user.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new NotFoundByIdException("User with id '" + id + "' not found"));
