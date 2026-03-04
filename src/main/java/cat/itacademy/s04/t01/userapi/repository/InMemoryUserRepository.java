dcc2e623b3852fa6e2c5b8ffa9a7eb4926885eab
Federico Cantore <f.cantore85@gmail.com>
2026-03-04 12:24:14 +0100

    feat: created InMemoryUserRepository implementation

--- src/main/java/cat/itacademy/s04/t01/userapi/repository/InMemoryUserRepository.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/repository/InMemoryUserRepository.java
@@ -1,4 +1,36 @@
 package cat.itacademy.s04.t01.userapi.repository;
 
-public class InMemoryUserRepository {
+import cat.itacademy.s04.t01.userapi.entity.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class InMemoryUserRepository implements UserRepository {
+    @Override
+    public User save(User user) {
+        return null;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<User> findById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<User> searchByName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return false;
+    }
 }
