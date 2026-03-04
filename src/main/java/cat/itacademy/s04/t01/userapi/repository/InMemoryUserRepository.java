9ce22a17bf4b789ac11cb2500d47ed8972f12a34
Federico Cantore <f.cantore85@gmail.com>
2026-03-04 12:56:11 +0100

    refator: moved users list in InMemoryUserRepository and make it private
    feat: implemented save()

--- src/main/java/cat/itacademy/s04/t01/userapi/repository/InMemoryUserRepository.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/repository/InMemoryUserRepository.java
@@ -1,17 +1,27 @@
 package cat.itacademy.s04.t01.userapi.repository;
 
 import cat.itacademy.s04.t01.userapi.entity.User;
+import cat.itacademy.s04.t01.userapi.exception.NotFoundByIdException;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
+
+    private final List<User> users = new ArrayList<User>();
+
     @Override
     public User save(User user) {
-        return null;
+        if(user.getId() == null) {
+            user.setId(UUID.randomUUID());
+        }
+
+        users.add(user);
+        return user;
     }
 
     @Override
@@ -21,6 +31,10 @@ public class InMemoryUserRepository implements UserRepository {
 
     @Override
     public Optional<User> findById(UUID id) {
+        users.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new NotFoundByIdException("User with id '" + id + "' not found"));
         return Optional.empty();
     }
 
