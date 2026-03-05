917084b62216b6afa96db726b26ab2b45c2a65e6
Federico Cantore <f.cantore85@gmail.com>
2026-03-05 15:58:10 +0100

    feat: extended UserServiceImpl with MockitoExtension

---
+++ src/test/java/cat/itacademy/s04/t01/userapi/service/UserServiceImplTest.java
@@ -0,0 +1,19 @@
+package cat.itacademy.s04.t01.userapi.service;
+
+import cat.itacademy.s04.t01.userapi.repository.UserRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceImplTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+
+}
