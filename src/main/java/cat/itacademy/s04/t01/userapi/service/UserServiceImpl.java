f2053545f99a3ba8772083db452bbd1db6e76771
Federico Cantore <f.cantore85@gmail.com>
2026-03-05 10:46:23 +0100

    feat: implemented UserService in UserServiceImpl

--- src/main/java/cat/itacademy/s04/t01/userapi/service/UserServiceImpl.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/service/UserServiceImpl.java
@@ -1,4 +1,42 @@
 package cat.itacademy.s04.t01.userapi.service;
 
-public class UserServiceImpl {
+import cat.itacademy.s04.t01.userapi.entity.User;
+import cat.itacademy.s04.t01.userapi.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User createUser(User user) {
+        return null;
+    }
+
+    @Override
+    public List<User> listUsers(String name) {
+        return List.of();
+    }
+
+    @Override
+    public User getByID(UUID id) {
+        return null;
+    }
+
+    @Override
+    public List<User> searchUsersByName() {
+        return List.of();
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return false;
+    }
 }
