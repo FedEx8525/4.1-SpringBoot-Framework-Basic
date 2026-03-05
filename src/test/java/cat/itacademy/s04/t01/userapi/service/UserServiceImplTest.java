9e72a5990a154432ff1f309a443ddf66f68a486a
Federico Cantore <f.cantore85@gmail.com>
2026-03-05 16:11:34 +0100

    feat: add createUser_shouldThrowExceptionWhenEmailAlreadyExists test

--- src/test/java/cat/itacademy/s04/t01/userapi/service/UserServiceImplTest.java
+++ src/test/java/cat/itacademy/s04/t01/userapi/service/UserServiceImplTest.java
@@ -1,10 +1,16 @@
 package cat.itacademy.s04.t01.userapi.service;
 
+import cat.itacademy.s04.t01.userapi.entity.User;
+import cat.itacademy.s04.t01.userapi.exception.EmailAlreadyExistsException;
 import cat.itacademy.s04.t01.userapi.repository.UserRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceImplTest {
@@ -14,6 +20,19 @@ public class UserServiceImplTest {
 
     @InjectMocks
     private UserServiceImpl userService;
+
+    @Test
+    void createUser_shouldThrowExceptionWhenEmailAlreadyExists() {
+        User user = new User();
+        user.setEmail("test@mail.com");
+
+        when(userRepository.existsByEmail("test@mail.com")).thenReturn(true);
+
+        assertThrows(EmailAlreadyExistsException.class, () -> {
+            userService.createUser(user);
+        });
+        verify(userRepository, never()).save(any(User.class));
+    }
 
 
 }
